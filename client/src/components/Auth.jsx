b46040c7da1cb3cf757d19aefd0319ab4a7785d2
AhFa <contactocoachahfa@gmail.com>
2023-10-01 15:17:37 +0200

    first commit

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Auth() {
+  return (
+    <div className='w-full h-screen flex justify-center pt-[5rem] bg-gradient-to-r from-cyan-500 to-blue-500'>
+        <h1 className='font-bold sm:text-[3.5vw] text-[2vw]'>Welcome to TrackSelf!</h1>
+    </div>
+  )
+}
+
+export default Auth
